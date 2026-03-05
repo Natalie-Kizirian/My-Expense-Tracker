fd994fcb9a93e338f3c18d43afe4e8d7667861a6
Natalie Kizirian <natalie.kizirian@gmail.com>
2026-03-05 09:42:13 +0200

    Set Up

---
+++ script.js
@@ -0,0 +1,4 @@
+const categoryInput = document.getElementById("category-input");
+const amountInput = document.getElementById("amount-input");
+const incomeBtn = document.getElementById("income-btn");
+const expenseBtn = document.getElementById("expense-btn");
